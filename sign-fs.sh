2c62c3e389b7f8c41cc7a46b3bead71e7c669d9a
Woodrow Shen <woodrow.shen@canonical.com>
2017-06-07 10:39:18 +0800

    Minimize local mirror - filter RSYNC_EXCLUDE from anonftpsync config - add RSYNC_INCLUDE suport for anonftpsync - add --include-from support for anonftpsync

---
+++ sign-fs.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+echo "[BUILD] - Generate new filesystem.squashfs with the updated ubuntu-archive-keyring.gpg"
+sudo rm temp -rf filesystem.squashfs
+mkdir temp
+cd temp
+sudo unsquashfs ../$1
+cd squashfs-root
+sudo cp ~/ubuntu-keyring/ubuntu-keyring-2012.05.19/keyrings/ubuntu-archive-keyring.gpg usr/share/keyrings/ubuntu-archive-keyring.gpg
+sudo cp ~/ubuntu-keyring/ubuntu-keyring-2012.05.19/keyrings/ubuntu-archive-keyring.gpg etc/apt/trusted.gpg
+sudo cp ~/ubuntu-keyring/ubuntu-keyring-2012.05.19/keyrings/ubuntu-archive-keyring.gpg var/lib/apt/keyrings/ubuntu-archive-keyring.gpg
+sudo mksquashfs ./ ../../filesystem.squashfs
+sudo chown $USER:$USER ../../filesystem.squashfs
+sudo du -sx --block-size=1 ./ | cut -f1 > ../../filesystem.size 
+
