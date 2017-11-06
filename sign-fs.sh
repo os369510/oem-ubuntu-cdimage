4ab3dd09319ccd7c89e9c4d5711bcc1dddb4e573
Woodrow Shen <woodrow.shen@canonical.com>
2017-11-07 03:24:26 +0800

    Modify the path of keyring for Argos

--- sign-fs.sh
+++ sign-fs.sh
@@ -6,9 +6,9 @@ mkdir temp
 cd temp
 sudo unsquashfs ../$1
 cd squashfs-root
-sudo cp ~/ubuntu-keyring/ubuntu-keyring-2012.05.19/keyrings/ubuntu-archive-keyring.gpg usr/share/keyrings/ubuntu-archive-keyring.gpg
-sudo cp ~/ubuntu-keyring/ubuntu-keyring-2012.05.19/keyrings/ubuntu-archive-keyring.gpg etc/apt/trusted.gpg
-sudo cp ~/ubuntu-keyring/ubuntu-keyring-2012.05.19/keyrings/ubuntu-archive-keyring.gpg var/lib/apt/keyrings/ubuntu-archive-keyring.gpg
+sudo cp ~/Argos/ubuntu-keyring/ubuntu-keyring-2012.05.19/keyrings/ubuntu-archive-keyring.gpg usr/share/keyrings/ubuntu-archive-keyring.gpg
+sudo cp ~/Argos/ubuntu-keyring/ubuntu-keyring-2012.05.19/keyrings/ubuntu-archive-keyring.gpg etc/apt/trusted.gpg
+sudo cp ~/Argos/ubuntu-keyring/ubuntu-keyring-2012.05.19/keyrings/ubuntu-archive-keyring.gpg var/lib/apt/keyrings/ubuntu-archive-keyring.gpg
 sudo mksquashfs ./ ../../filesystem.squashfs
 sudo chown $USER:$USER ../../filesystem.squashfs
 sudo du -sx --block-size=1 ./ | cut -f1 > ../../filesystem.size 
