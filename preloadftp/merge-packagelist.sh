e9ab60c00ab0cfa7ae7d3876913684502b3d1377
Woodrow Shen <woodrow.shen@canonical.com>
2017-11-16 16:03:37 +0800

    Optimize package list to minimize ftp mirror

---
+++ preloadftp/merge-packagelist.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+cat server-pool-arch.list desktop-pool-arch.list | sort -u > xenial-classic-arch.list
+sed 's/\*_\(amd64\|all\).deb//g' xenial-classic-arch.list > xenial-classic-pkgname.list
+
+./update-packagelist && cat udebs.list xenial-classic-apt.list > xenial-classic.list
