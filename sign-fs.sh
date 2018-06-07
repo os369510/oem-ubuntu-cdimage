16c3d2d8a47f52048322fde7389c70edeb5effe9
Woodrow Shen <woodrow.shen@canonical.com>
2018-06-07 06:34:29 +0000

    Use newer version for ubuntu-keyring in bionic

--- sign-fs.sh
+++ sign-fs.sh
@@ -6,8 +6,8 @@ mkdir temp
 cd temp
 sudo unsquashfs ../$1
 cd squashfs-root
-sudo cp $2/ubuntu-keyring-2012.05.19/keyrings/ubuntu-archive-keyring.gpg usr/share/keyrings/ubuntu-archive-keyring.gpg
-sudo cp $2/ubuntu-keyring-2012.05.19/keyrings/ubuntu-archive-keyring.gpg etc/apt/trusted.gpg
+sudo cp $2/ubuntu-keyring-2018.02.28/keyrings/ubuntu-archive-keyring.gpg usr/share/keyrings/ubuntu-archive-keyring.gpg
+sudo cp $2/ubuntu-keyring-2018.02.28/keyrings/ubuntu-archive-keyring.gpg etc/apt/trusted.gpg
 sudo mksquashfs ./ ../../filesystem.squashfs
 sudo chown $USER:$USER ../../filesystem.squashfs
 sudo du -sx --block-size=1 ./ | cut -f1 > ../../filesystem.size 
